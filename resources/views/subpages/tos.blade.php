6b0fb2215dab5651f87b741a2e0f3f677e4ec4fe
Roman Schabus <54069085+DaWoschbar@users.noreply.github.com>
2020-03-31 09:31:54 +0200

    Updated contacts

--- resources/views/subpages/tos.blade.php
+++ resources/views/subpages/tos.blade.php
@@ -7,17 +7,17 @@
         <div class="card-header">{{ __('Information') }}</div>
 
         <div class="card-body">
-            <h1 class="adsimple-221105344">Impressum</h1>
+            <h1 class="adsimple-221105344">Terms of services</h1>
             <p class="adsimple-221105344">Duty to provide information in accordance with Section 5 of the E-Commerce Act, Section 14 of the German Business Code, Section 63 of the Commercial Code and disclosure requirements under Section 25 of the Media Act.</p>
             <p class="adsimple-221105344">
-            <p><strong>Contact:</strong> Reiner Rabensteiner</p>
-            <strong>E-Mail:</strong> <a href="mailto:reiner.rabensteiner@htl-villach.at"> reiner.rabensteiner@htl-villach.at</a>
+            <p><strong>Contact:</strong> HTL Villach</p>
+            <strong>E-Mail:</strong> <a href="mailto:systemadministration@htl-villach.at"> systemadministration@htl-villach.at</a>
             </p>
             <p class="adsimple-221105344">HTL-Villach <br>Tschinowitscher Weg 5 <br />9500 Villach <br />Austria</p>
             <p class="adsimple-221105344">
             <p>
                 <h1>WhatTheHack Development team</h1>
-            <p>This platform was developed by students of 5AHITN in 2019/20 as part of the ITP lessons.</p>
+            <p>This platform was developed by students of 5AHITN in 2019/20 as part of the ITP school lessons.</p>
             Roman Schabus - <a href="https://twitter.com/woschbar">@DaWoschbar</a><br>
             Darius Haslauer -<a href="https://github.com/Buschuschnig">@Buschuschnig</a> <br>
             Sandro Omann - @Sanjey_Sandmann<br>
